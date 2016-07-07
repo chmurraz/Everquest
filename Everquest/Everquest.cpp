f2815089999899077580a070e73c93b40ee3821a
Chris <chris.murray.az@gmail.com>
2016-07-06 22:59:42 -0700

    Made some minor changes, trying to improve GUI and clean up code/classes

--- Everquest/Everquest.cpp
+++ Everquest/Everquest.cpp
@@ -24,24 +24,35 @@ namespace SimulateKeyPress
 	public ref class Form1 : public Form
 	{
 	public:
-		Form1(char ch)
+		Form1()
 		{
 			// The ^ hat symbol here is a garbage collection thing from Microsoft https://msdn.microsoft.com/en-us/library/yk97tc08.aspx
 			// gcnew (or garbage collection new) is also a Microsoft thing https://msdn.microsoft.com/en-us/library/te3ecsc8.aspx
 			// These require CLR compiler option
+			Text = "Everquest Botter Thingy!";
 
 			Button^ button1 = gcnew Button();
+			Button^ button2 = gcnew Button();
 			button1->Location = Point(10, 10);
+			button2->Location = Point(10, 40);
+
+			//	This will be the "zeroth" button when pressing tab to cycle between them
 			button1->TabIndex = 0;
+			button2->TabIndex = 1;
 			button1->Text = "Click to automate EQ";
+			button2->Text = "Do something else";
 			button1->AutoSize = true;
+			button2->AutoSize = true;
 			button1->Click += gcnew EventHandler(this, &Form1::button1_Click);
+			button2->Click += gcnew EventHandler(this, &Form1::button2_Click);
 			this->Controls->Add(button1);
+			this->Controls->Add(button2);
 		}
 
 	// Execute commands when button is pressed.
 	private:
 		void button1_Click(Object^ sender, EventArgs^ e);
+		void button2_Click(Object^ sender, EventArgs^ e);
 	};
 	inline void Form1::button1_Click(Object ^ sender, EventArgs ^ e)
 	{
@@ -54,12 +65,16 @@ namespace SimulateKeyPress
 		logWatcher->setCharacter(Necro);
 		Necro->NecroRoutine();
 	}
+	void Form1::button2_Click(Object ^ sender, EventArgs ^ e)
+	{
+		MessageBox::Show("Ouch!");
+	}
 }
 
 [STAThread]
 int main()
 {
-	Console::WriteLine("testing (in Everquest.cpp:main) 1 2 3");
+	Console::WriteLine("testing (in Everquest.cpp:main) 1 2 3... this is a write line to console");
 	Application::EnableVisualStyles();
-	Application::Run(gcnew SimulateKeyPress::Form1('\r'));
+	Application::Run(gcnew SimulateKeyPress::Form1());
 }
